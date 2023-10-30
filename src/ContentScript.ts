1c1b1d93281774bb8c5548489985edc2d62cb432
AshotManukyan <69624036+Ashot-hex@users.noreply.github.com>
2023-10-30 07:09:30 +0100

    Storage renommé en ChromeStorage, et implementation de ChromeStorage.Load et ChromeStorage.Save

--- src/ContentScript.ts
+++ src/ContentScript.ts
@@ -3,22 +3,15 @@ import { PageParser } from "./Model/LogicLayer/Parsing/PageParser";
 import { SemesterNames } from "./Model/LogicLayer/Parsing/SemesterNames";
 import { Semestre } from "./Model/Types/Grades/Elements/Semestre";
 import { MainPageView } from "./View/MainPageView";
-import { Storage } from "./Data/Storage/Storage";
+import { ChromeStorage } from "./Data/Storage/Storage";
 import { StoredSemester } from "./Model/Types/Storage/StoredSemester";
 
 let semester: Semestre = SemestreFactory.Instance.GetSemester();
 // chrome.storage.local.clear();
 
-Storage.Save(semester);
+new ChromeStorage().Save(semester);
 
-Storage.Load().then((savedSemesters) => {
-    console.log(savedSemesters);
-    let currentSemestre: StoredSemester = savedSemesters[SemesterNames.CurrentSemestre];
-    let correspondingSemestre: StoredSemester = savedSemesters[SemesterNames.CorrespondingSemester];
-    for (let i = 0; i < currentSemestre.Averages.length; i++){
-        console.log((currentSemestre.Averages[i] + correspondingSemestre.Averages[i]) / 2);
-    }
-});
+console.log(new ChromeStorage().Load());
 
 // if (!PageParser.Instance.AreGradesShown)
 // {
--- src/Data/Storage/Storage.ts
+++ src/Data/Storage/Storage.ts
@@ -3,32 +3,40 @@ import { SemesterNames } from "../../Model/LogicLayer/Parsing/SemesterNames";
 import { Semestre } from "../../Model/Types/Grades/Elements/Semestre";
 import { StoredSemester } from "../../Model/Types/Storage/StoredSemester";
 
-export class Storage implements IStorage
-{
-    //#region Singleton
-    private constructor() {}
-    private static _instance: Storage;
-    public static get Instance() { return this._instance || (this._instance = new this()); }
-    //#endregion Singleton
-    
+export class ChromeStorage implements IStorage {
+    private cache: { [id: string]: StoredSemester; } = {};
+
+    public constructor() {
+        // Initialize cache from Chrome's storage
+        this.initCache();
+    }
+
     public Save(semester: Semestre): void {
-        this.Load().then((savedSemesters) => {
-            savedSemesters[SemesterNames.CurrentSemestre] = semester.ToStoredSemester();
-            this.AsyncSave(savedSemesters);
-        });
+        // Assuming semester has an 'id' field that is a string
+        this.cache[SemesterNames.CurrentSemestre] = semester.ToStoredSemester();
+        // Save the cache asynchronously
+        this.AsyncSave();
     }
+
     public Load(): { [id: string]: StoredSemester; } {
-        throw new Error("Not impl");
+        return this.cache;
     }
-    public async AsyncLoad(): Promise<{ [id: string]: StoredSemester; }> {
+
+    //#region Private
+    private async initCache(): Promise<void> {
+        this.cache = await this.AsyncLoad();
+    }
+
+    private async AsyncLoad(): Promise<{ [id: string]: StoredSemester; }> {
         return new Promise((resolve) => {
             chrome.storage.local.get(["SavedSemesters"], function (result) {
                 resolve(result["SavedSemesters"] || {});
             });
         });
-    } 
-    
-    private async AsyncSave(semestersToSave: { [id: string]: StoredSemester; }): Promise<void> {
-        chrome.storage.local.set({ "SavedSemesters": semestersToSave });
     }
+
+    private async AsyncSave(): Promise<void> {
+        chrome.storage.local.set({ "SavedSemesters": this.cache });
+    }
+    //#endregion Private
 }
