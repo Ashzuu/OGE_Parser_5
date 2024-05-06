b2782a346fae69d183869cb8b1ed415bd37b3d55
AshotManukyan <ashotm0571@gmail.com>
2024-05-06 08:17:27 +0200

    Suppression des property et constantes inutiles, et extraction de l'affichage de Content.Setup à Content.Display

--- src/Content.ts
+++ src/Content.ts
@@ -5,32 +5,11 @@ import { Injector } from "./DependencyInjector";
 
 /** Gestion du contenu de la page principale */
 export class Content {
-    //#region constants
-    private readonly LOADING_ICON_CLASS: string = 'ui-dialog ui-widget ui-widget-content ui-corner-all ui-shadow ui-hidden-container statusDialog';
-    private readonly SEMESTER_LINKS_CLASS: string = 'ui-menuitem-link ui-corner-all';
-    //#endregion constants
-
-    //#region Properties
-    private get SemesterLinks(): HTMLElement[] {
-        return <HTMLElement[]>Array.from(
-            document.getElementsByClassName(this.SEMESTER_LINKS_CLASS)
-        );
-    }
-    private get LoadingIcon(): HTMLElement {
-        return <HTMLElement>document.getElementsByClassName(this.LOADING_ICON_CLASS)[0];
-    }
-    //#endregion Properties
-
     /** Met en place le traitement de la page */
     public Setup(): void {
         //Parsing de la page
         const semester = this.ProcessSemester();
-        
-        const gradeDisplay = Injector.GradeDisplay;
-        //Changement de la phrase Remarque
-        gradeDisplay.DisplayWarning();
-        //Affichage des moyennes
-        gradeDisplay.DisplayGrades(semester);
+        this.Display(semester);
     }
 
     // Parse la page actuelle, sauvegarde les resultats, et les affiche
@@ -39,5 +18,13 @@ export class Content {
         GradeParser.Reset();
         //Parsing de la page
         return new SemesterFactory().GetSemester() ?? new Error("Semestre non trouvé");
+    }
+
+    private Display(semester: Semestre){
+        const gradeDisplay = Injector.GradeDisplay;
+        //Changement de la phrase Remarque
+        gradeDisplay.DisplayWarning();
+        //Affichage des moyennes
+        gradeDisplay.DisplayGrades(semester);
     }
 }
